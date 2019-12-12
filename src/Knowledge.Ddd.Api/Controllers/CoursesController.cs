9a5498816dfc06c03d5edaaf2d9803748e235b9f
Otaviano Montes Zibetti <otavianozibetti@rede.social>
2019-12-12 13:14:42 -0300

    Added api project

---
+++ src/Knowledge.Ddd.Api/Controllers/CoursesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Knowledge.Ddd.Application.Interfaces;
+using Knowledge.Ddd.Application.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Knowledge.Ddd.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseService courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        // POST: api/Courses
+        [HttpPost]
+        public IActionResult Post([FromBody] CourseViewModel model)
+        {
+            courseService.Create(model);
+
+            return Ok(model);
+        }
+    }
+}
---
+++ src/Knowledge.Ddd.Api/Startup.cs
@@ -0,0 +1,49 @@
+using Knowledge.Ddd.Infra.IoC;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Knowledge.Ddd.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(p => p.SwaggerDoc("v1", new OpenApiInfo{ Title = "University API", Version = "v1"}));
+            services.AddMediatR(typeof(Startup));
+            
+            DependencyContainer.RegisterServices(Configuration, services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(p => p.SwaggerEndpoint("/swagger/v1/swagger.json", "University API v1"));
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
